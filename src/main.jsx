f60b00d9622fcf33f736f4649cf4279bc0ad2b9a
Chaitanya Agarkar <129309578+chaieeetanya433@users.noreply.github.com>
2023-10-07 14:46:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,14 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-quill/dist/quill.snow.css";
 import "./index.scss";
+import { AuthProvider } from "./AuthContext";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
     <ToastContainer />
+    </AuthProvider>
   </React.StrictMode>
 );
