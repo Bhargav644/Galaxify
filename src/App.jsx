22b5ca2de151f94985e52fca92ab8fa52b09cd9b
bhargav sharma1 <bhargav.sharma1@bfhl.in>
2023-10-07 10:29:08 +0530

    Basic Repo Structure has been added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter,Route,Router } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route path="/"/>
+      </Router>
+    
+    </BrowserRouter>
+       
+  )
+}
+
+export default App
