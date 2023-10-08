67fa5bfc3e96674dd5ddd651c7f2dac8ad3c87b5
reshav.rana <reshav.rana@bfhl.in>
2023-10-08 19:05:17 +0530

    gollu is done with quiz form and generating a personalized package for user

--- src/components/form-pages/SuccessRedirection.jsx
+++ src/components/form-pages/SuccessRedirection.jsx
@@ -2,11 +2,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ActivitySelector from "./partial/activity-selector";
+import { successWrapper } from "./styles";
 
 const SuccessRedirection = ({ data }) => {
   const url = "https://galaxify-bacend.vercel.app/api/recommend";
   const [packageData, setPackageData] = useState([]);
+  const [packageName, setPackageName] = useState("");
   const [errorState, setError] = useState({});
+  const [currName, setCurrName] = useState("");
+  const onChange = (e) => {
+    const letter = e.target.value;
+    setCurrName((prev) => ({ ...prev, letter }));
+  };
+  const onClick = () => {
+    setPackageName(currName);
+  };
 
   useEffect(() => {
     axios({
@@ -27,11 +37,17 @@ const SuccessRedirection = ({ data }) => {
       });
   }, [errorState, data]);
 
-  console.log("reshav", data, packageData);
   return (
-    <div>
+    <div className={successWrapper}>
       <p> Well Done! Your Quiz Has Been Submitted Successfully </p>
-      {packageData.length && <ActivitySelector activity={packageData[0]} />}
+      <div>
+        <input type="text" onChange={onChange} />
+        <button onClick={onClick}>Done</button>
+      </div>
+
+      {packageData.length && packageName && (
+        <ActivitySelector activity={packageData[0]} packageName={packageName} />
+      )}
     </div>
   );
 };
--- src/modules/constant.js
+++ src/modules/constant.js
@@ -5,9 +5,9 @@ import Mercury from "../assets/Mercury.jpeg";
 import Moon from "../assets/Moon.jpeg";
 import Neptune from "../assets/Neptune.jpeg";
 import Uranus from "../assets/Uranus.jpg";
-import Venus from "../assets/Venus.jpeg"
-import Mars from "../assets/Mars.jpeg"
-import Fallback from "../assets/Fallback.jpeg"
+import Venus from "../assets/Venus.jpeg";
+import Mars from "../assets/Mars.jpeg";
+import Fallback from "../assets/Fallback.jpeg";
 
 export const theme = {
   dark: "#111111",
@@ -17,21 +17,22 @@ export const theme = {
   logoColorLight: "rgba(255,255,255,0.6)",
   grey: "#BDAFBE",
   blue: "#17366F",
+  secondaryDark: "#494F55",
 };
 
 export const baseURL = "https://galaxify-bacend.vercel.app/api";
 
-export const Planets={
-    Earth:Earth,
-    Moon:Moon,
-    Mercury:Mercury,
-    Mars:Mars,
-    Neptune:Neptune,
-    Venus:Venus,
-    Jupiter:Jupiter,
-    Uranus:Uranus,
-    Saturn:Saturn,
-    Titan:Venus,
-    Fallback:Fallback,
-    Cupid:Moon,
-}
+export const Planets = {
+  Earth: Earth,
+  Moon: Moon,
+  Mercury: Mercury,
+  Mars: Mars,
+  Neptune: Neptune,
+  Venus: Venus,
+  Jupiter: Jupiter,
+  Uranus: Uranus,
+  Saturn: Saturn,
+  Titan: Venus,
+  Fallback: Fallback,
+  Cupid: Moon,
+};
