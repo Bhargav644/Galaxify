70024f718184c0436a7f5c96c237d26736f526db
bhargav sharma1 <bhargav.sharma1@bfhl.in>
2023-10-08 12:32:24 +0530

    Reshav Bug Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/landing";
+import LandingPage from "../pages/landing";
 
 function App() {
   return (
