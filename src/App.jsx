81b9e9afcc43a0e024fa3fe863a689c84dd1fe90
bhargav sharma1 <bhargav.sharma1@bfhl.in>
2023-10-08 12:57:33 +0530

    branching fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing";
+import DestinationsPage from "./pages/Destinations";
+import ProductDescriptionPage from "./pages/PDP";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/destinations" element={<DestinationsPage/>}/>
+        <Route path="/destinations/:destination" element={<ProductDescriptionPage/>}/>
         <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
